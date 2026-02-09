a91dd19ec07239e4fe8dd9b1da40fcb3681970e8
Sam Saju <samsaju04@gmail.com>
2026-02-09 07:15:08 +0000

    updates

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker compose up -f docker-compose.yml --abort-on-container-exit
-docker compose down
+docker compose -f /home/samsaju/cloud-stuff/docker-compose.yml up --abort-on-container-exit
+docker compose -f /home/samsaju/cloud-stuff/docker-compose.yml down
